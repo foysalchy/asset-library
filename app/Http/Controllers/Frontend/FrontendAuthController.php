e4fd68fe91b6d8e4453d668169b53d01a5174bee
kalam714 <kalamahmed714@gmail.com>
2026-08-24 09:55:37 +0600

    site lgoo

--- app/Http/Controllers/Frontend/FrontendAuthController.php
+++ app/Http/Controllers/Frontend/FrontendAuthController.php
@@ -230,9 +230,7 @@ class FrontendAuthController extends Controller
                     'subject' => 'Reset Your Password',
                     'data'    => [
                         'site_name' => optional($settings)->site_name ?? config('app.name'),
-                        'site_logo' => 'https://asset.bhaiyahousing.com/storage/settings/2043f208-bd04-4331-8894-0f6f1fc83d14.png'
-                            ? (str_starts_with($settings->logo, 'http') ? $settings->logo : asset($settings->logo))
-                            : asset('img/logo.png'),
+                        'site_logo' => 'https://asset.bhaiyahousing.com/storage/settings/2043f208-bd04-4331-8894-0f6f1fc83d14.png',
                         'eyebrow'           => 'Password Reset',
                         'heading'           => 'Reset your password',
                         'name'              => $user->name,
